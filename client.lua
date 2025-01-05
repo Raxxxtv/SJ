8c71c513804d0f7d459c8fa800b6365de364056a
Raxxxtv <raxtv.mail@gmail.com>
2025-01-05 17:21:40 +0100

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,17 @@
+local model = 'g_m_importexport_01'
+local superjumpactive = false
+
+RegisterCommand("SJ", function()
+    if IsModelInCdimage(model) and IsModelValid(model) then
+        RequestModel(model)
+        while not HasModelLoaded(model) do
+            Wait(0)
+        end
+        SetPlayerModel(PlayerId(), model)
+        superjumpactive = true
+        while superjumpactive == true do
+            SetSuperJumpThisFrame(PlayerId())
+            Wait(1)
+        end
+    end
+end)
